eb6a841fdb7de30f3c280b720b030edf10960470
PiuSen <peusen113@gmail.com>
2023-05-18 11:12:38 +0530

    changes

--- app/src/main/java/com/example/images/Module/ImageInterface.java
+++ app/src/main/java/com/example/images/Module/ImageInterface.java
@@ -1,5 +1,7 @@
 package com.example.images.Module;
 
+import androidx.room.Database;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
@@ -10,7 +12,7 @@ public interface ImageInterface {
     @Headers({"Authorization: fpeHL7EyTvpo0YeeG7BBlgPHrNym55jG5mx1EHhyDxGn7yKPIJROvPbe"})
     @GET("curated")
     Call<Model>getModel(@Query("per_page")int perpage);
-
-
-
+    @Headers({"Authorization:fpeHL7EyTvpo0YeeG7BBlgPHrNym55jG5mx1EHhyDxGn7yKPIJROvPbe"})
+    @GET("search")
+    Call<Model>getApiReaspons(@Query("query")String natureName ,@Query("per_page")int perpage);
 }
