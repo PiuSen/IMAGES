0f5d8a13e0c5df47e74f535cbdcd4ae352370390
PiuSen <peusen113@gmail.com>
2023-07-28 00:33:10 +0530

    updated

--- app/src/main/java/com/example/images/MainActivity.java
+++ app/src/main/java/com/example/images/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.images;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.images.Module.Adapter;
 import com.example.images.Module.ImageInstance;
 import com.example.images.Module.ImageInterface;
 import com.example.images.Module.Model;
 import com.example.images.Module.Photo;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,41 +25,49 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerImage;
-    List<Photo>photos=new ArrayList<>();
-    Adapter adapter;
 
+    BottomNavigationView bottomNavigationView;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerImage=findViewById(R.id.RecyclerImage);
 
 
-        adapter=new Adapter(this,photos);
-        recyclerImage.setLayoutManager( new GridLayoutManager(this,2 ));
-        recyclerImage.setAdapter(adapter);
+        bottomNavigationView=findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        replaceFragment(new HomeFragment());
 
-        ImageInterface imageInterface= ImageInstance.getImageInterface();
-        Call<Model> response=imageInterface.getModel(60);
-
-        response.enqueue(new Callback<Model>() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onResponse(Call<Model> call, Response<Model> response) {
-                if(response.isSuccessful()){
-                    photos.addAll(response.body().getPhotos());
-                    adapter.notifyDataSetChanged();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        replaceFragment(new HomeFragment());
+                                
+                    break;
+                    case R.id.searlist:
+                        replaceFragment(new SearchFragment());
+                        
+                        break;
+                    case  R.id.myfavourite:
+                        replaceFragment(new FavouriteFragment());
+                        break;
+                    default:
+                        replaceFragment(new HomeFragment());
+                        break;
+
                 }
-            }
-
-
-            @Override
-            public void onFailure(Call<Model> call, Throwable t) {
-
+                return true;
             }
         });
 
 
-        }
-
     }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+    }
+}
